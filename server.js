ab5a25850c28783fdedf67221c7071aad21ba620
Nathan Mc <nathanmcneil@pm.me>
2020-05-28 23:46:50 -0700

    created initial files and added packages

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+app.use(require('./routes'))
+
+
+app.listen(process.env.PORT, () => console.log('http://localhost:3000'))
